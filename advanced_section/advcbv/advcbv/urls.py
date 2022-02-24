0beae72a04152d9ba945e461e0c5e65462e38bb5
a-ge <amgesmundo@gmail.com>
2022-02-23 16:00:13 -0800

    context dict with templateview

--- advanced_section/advcbv/advcbv/urls.py
+++ advanced_section/advcbv/advcbv/urls.py
@@ -19,6 +19,6 @@ from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.CBView.as_view()),
+    path('', views.IndexView.as_view()),
     # path('', views.index, name='index'),
 ]
--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 
-# Gritty. more manual way to do CBV
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("CLASS BASED VIEWS ARE COOL!")
+class IndexView(TemplateView):
+    # template_name: class object attribute
+    template_name = 'index.html'
+
+    # use context dict with TemplateView
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION'
+        return context
+
+# # Gritty. more manual way to do CBV
+# class CBView(View):
+#     def get(self, request):
+#         return HttpResponse("CLASS BASED VIEWS ARE COOL!")
 
 
 # # Create your views here.
